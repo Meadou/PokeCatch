24cdeccd4d97e027cda340e81659f6782566595a
Villarante <rmvillarante03202400130@usep.edu.ph>
2025-12-06 19:53:02 +0800

    Trail ni Raven

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,7 @@ public class Main {
         // task 3 Shiny Magicarp
         // task 4 bulbasaur
         System.out.println("Welcome to PokeCatch!");
+        System.out.println("marjhun gaming");
+
     }
 }

bcb917bdf1f862010f20385cb23299cbc13b326c
Marjhun <marjhunkyle.pamilar@gmail.com>
2025-12-13 23:29:22 +0800

    big update

---
+++ src/Model/Types.java
@@ -0,0 +1,20 @@
+package Model;
+
+public enum Types {
+    NORMAL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    STEEL,
+}
